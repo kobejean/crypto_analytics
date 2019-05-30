382b2d77e87cb183ecc32374268561882ee72781
Jacoab <jjongewaard1@gmail.com>
2019-05-29 18:08:49 -0600

    Added CoinMarketCap class to data_sources and added a corresponding collection file

---
+++ src/collection/cmc_collect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import json
+
+from __context__ import crypto_analytics
+from crypto_analytics.collection.data_source import CoinMarketCap
+
+params = input('Parameters: ')
+params = json.loads(params)
+endpoint = input('Endpoint: ')
+key = input('Key: ')
+output_file = input('Output File: ')
+
+coin_market_cap = CoinMarketCap(key, endpoint)
+coin_market_cap.fetch()
+coin_market_cap.write(output_file)
