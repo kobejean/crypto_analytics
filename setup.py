9a6e6181b27eef6bec170574f64bfa632c5bd950
Jean Flaherty <kobejean@me.com>
2019-06-01 11:39:03 -0600

    package set up and direct module imports

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='crypto_analytics',
+      version='0.0',
+      # description='',
+      url='http://github.com/Jacoab/crypto_analytics',
+      # author='',
+      # author_email='',
+      # license='',
+      packages=['crypto_analytics'],
+      zip_safe=False)
