ac3affc7963adc4fb058024abbe58a6c5504090a
Jean Flaherty <kobejean@me.com>
2019-06-08 19:20:17 -0600

    avoids abstractions for unit tests

--- tests/unit_tests/collection/test_cc_ohclv.py
+++ tests/unit_tests/collection/test_cc_ohclv.py
@@ -1,4 +1,3 @@
-import json
 import pandas as pd
 import os
 from pandas.util.testing import assert_frame_equal
@@ -6,18 +5,26 @@ from pandas.util.testing import assert_frame_equal
 from crypto_analytics.collection.data_source import CryptoCompareOHLCV
 from crypto_analytics.types import Interval
 
-def __get_json(relpath):
-    dir = os.path.dirname(__file__)
-    filepath = os.path.join(dir, relpath)
-    with open(filepath) as file:
-        return json.load(file)
+from mock_data import cc_ohclv_success, cc_ohclv_null_param
 
 def test_cc_ohlcv_fetch_success(requests_mock):
     # given
-    mock_response = __get_json('mock_data/cc_ohclv_success.json')
+    mock_response = cc_ohclv_success
     endpoint = 'https://min-api.cryptocompare.com/data/histominute'
     requests_mock.get(endpoint, json=mock_response)
     candles = CryptoCompareOHLCV(Interval.MINUTE, 'BTC', 'USD', 1)
+    # when
+    data = candles.fetch()
+    # then
+    expected_data = pd.DataFrame(mock_response['Data'])
+    assert_frame_equal(data, expected_data)
+
+def test_cc_ohlcv_fetch_failure(requests_mock):
+    # given
+    mock_response = cc_ohclv_null_param
+    endpoint = 'https://min-api.cryptocompare.com/data/histominute'
+    requests_mock.get(endpoint, json=mock_response)
+    candles = CryptoCompareOHLCV(Interval.MINUTE, None, 'USD', 1)
     # when
     data = candles.fetch()
     # then
