3fb3ec7fb594e7717b8b5f9927bb5aa2ee2d09d0
Jean Flaherty <kobejean@me.com>
2019-06-08 18:49:07 -0600

    code clean up

--- tests/unit_tests/collection/test_cc_ohclv.py
+++ tests/unit_tests/collection/test_cc_ohclv.py
@@ -6,19 +6,20 @@ from pandas.util.testing import assert_frame_equal
 from crypto_analytics.collection.data_source import CryptoCompareOHLCV
 from crypto_analytics.types import Interval
 
-def _get_json(relpath):
+def __get_json(relpath):
     dir = os.path.dirname(__file__)
     filepath = os.path.join(dir, relpath)
     with open(filepath) as file:
         return json.load(file)
 
 def test_cc_ohlcv_fetch_success(requests_mock):
-    mock_response = _get_json('mock_data/cc_ohclv_success.json')
-    expected_data = pd.DataFrame(mock_response['Data'])
+    # given
+    mock_response = __get_json('mock_data/cc_ohclv_success.json')
     endpoint = 'https://min-api.cryptocompare.com/data/histominute'
     requests_mock.get(endpoint, json=mock_response)
     candles = CryptoCompareOHLCV(Interval.MINUTE, 'BTC', 'USD', 1)
-
+    # when
     data = candles.fetch()
-
+    # then
+    expected_data = pd.DataFrame(mock_response['Data'])
     assert_frame_equal(data, expected_data)
