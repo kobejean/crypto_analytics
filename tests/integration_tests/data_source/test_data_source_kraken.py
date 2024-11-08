42acccf36c443b9c552262c6e41036af56ec1b96
Jean Flaherty <kobejean@me.com>
2024-11-09 00:07:10 +0900

    hard-2

--- tests/integration_tests/data_source/test_data_source_kraken.py
+++ tests/integration_tests/data_source/test_data_source_kraken.py
@@ -1,7 +1,7 @@
 import pytest, requests, re, os
 import numpy as np
 import pandas as pd
-from pandas.util.testing import assert_frame_equal, assert_series_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 from crypto_analytics.data_source import KrakenOHLCV
 from crypto_analytics.types import Interval
