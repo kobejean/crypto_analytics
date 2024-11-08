82f89e64d9961ce6f34217ff18180cf5a050d3d8
Jean Flaherty <kobejean@me.com>
2024-11-09 00:04:48 +0900

    easy bug 1

--- tests/unit_tests/types/test_interval.py
+++ tests/unit_tests/types/test_interval.py
@@ -10,16 +10,31 @@ test_unix_data = [
     (Interval.HOUR, 60*60),
     (Interval.DAY, 60*60*24),
 ]
-@pytest.mark.parametrize("interval,expected", test_unix_data)
-def test_interval_unix(interval, expected):
-    # when
-    unix_time = interval.unix
-    # then
-    assert unix_time == expected
 
 def test_inteval_second_unix():
     # when
     unix_time = Interval.SECOND.unix
     # then
     expected = 1
+    assert unix_time == expected
+
+def test_inteval_minute_unix():
+    # when
+    unix_time = Interval.MINUTE.unix
+    # then
+    expected = 60
+    assert unix_time == expected
+
+def test_inteval_hour_unix():
+    # when
+    unix_time = Interval.HOUR.unix
+    # then
+    expected = 60*60
+    assert unix_time == expected
+
+def test_inteval_day_unix():
+    # when
+    unix_time = Interval.DAY.unix
+    # then
+    expected = 60*60*24
     assert unix_time == expected
