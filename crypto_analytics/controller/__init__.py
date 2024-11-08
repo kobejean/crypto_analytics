20b1a0070f2b5b7e74f22adf5b774a6387ef3ade
Jean Flaherty <kobejean@me.com>
2024-11-09 03:57:56 +0900

    Update __init__.py

--- crypto_analytics/controller/__init__.py
+++ crypto_analytics/controller/__init__.py
@@ -2,7 +2,6 @@
 # base abstract classes
 from crypto_analytics.controller.base import Controller
 # other classes
-from crypto_analytics.controller.close_price import ClosePriceController
 from crypto_analytics.controller.collection import CollectionController
 
 __all__ = ['base', 'collection']
