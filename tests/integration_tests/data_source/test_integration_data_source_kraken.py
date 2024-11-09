57b89b4a588577e95de2cf9fe9dccaa3a65af87a
Jean Flaherty <kobejean@me.com>
2024-11-10 02:49:29 +0900

    t

--- tests/integration_tests/data_source/test_integration_data_source_kraken.py
+++ tests/integration_tests/data_source/test_integration_data_source_kraken.py
@@ -1,7 +1,7 @@
 import pytest, requests, re, os
 import numpy as np
 import pandas as pd
-from pandas.util.testing import assert_frame_equal, assert_series_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 from crypto_analytics.data_source import KrakenOHLCV
 from crypto_analytics.types import Interval
@@ -13,7 +13,7 @@ from crypto_analytics import utils
 
 k_ohclv_dtypes = {'time': np.int64, 'open': object, 'high': object, 'low': object, 'close': object, 'vwap': object, 'volume': object, 'count': np.int64 }
 k_ohclv_columns = ['time', 'open', 'high', 'low', 'close', 'vwap', 'volume', 'count']
-k_ohclv_past = [[1561939200, '10752.3', '11239.0', '9900.0', '10575.9', '10540.6', '14421.98699148', 47029], [1562025600, '10578.7', '10947.3', '9666.0', '10837.3', '10264.2', '15202.33829526', 50769]]
+k_ohclv_past = [[1730764800, '67787.8', '70482.0', '67481.4', '69382.7', '69409.3', '2008.23627213', 30812], [1730851200, '69382.7', '76457.6', '69341.0', '75642.0', '73794.8', '8946.48732813', 69815]]
 k_ohclv_past_df = pd.DataFrame(k_ohclv_past, columns=k_ohclv_columns).astype(k_ohclv_dtypes)
 
 # fetch method tests
@@ -22,13 +22,14 @@ def test_kraken_ohlcv_fetch_past():
     # given
     pair = SymbolPair(Symbol.BITCOIN, Symbol.USD)
     candles = KrakenOHLCV(Interval.DAY, pair, 2)
-    candles.to_time = 1562197365.6
+    candles.to_time = 1730937600.0
     # when
     data = candles.fetch()
     # then
     expected = k_ohclv_past_df
     # TODO: investigate why data is not consistent
-    assert_frame_equal(data.astype(np.float32), expected.astype(np.float32), check_less_precise=1)
+    print(data)
+    assert_frame_equal(data.astype(np.float32), expected.astype(np.float32), rtol=1e-6)
 
 def test_kraken_ohlcv_fetch_live():
     # given
